9c9cba29ef8393cf7b7da8ff4633b86f0cc8ea35
CyrilPonsan <cyril.ponsan@gmail.com>
2022-02-01 12:06:49 +0100

    abstraites

--- php/Animaux.classe.php
+++ php/Animaux.classe.php
@@ -1,51 +1,54 @@
 <?php
 
-abstract class Animaux {
+abstract class Animaux
+{
 
     private string $nom;
 
-    abstract function manger(int $nbreRepas) : string;
-    abstract function dormir() : void;
+    abstract function manger(int $nbreRepas): string;
+    abstract function dormir(): void;
 
     public function __construct(string $n)
     {
         $this->nom = $n;
     }
 
-    public function getNom() : string
+    public function getNom(): string
     {
         return $this->nom;
     }
 }
 
-class Terestres extends Animaux {
+class Terestres extends Animaux
+{
 
     public function manger(int $nbreRepas): string
     {
-        return parent::getNom(). " mange de tout ".$nbreRepas." fois par jour <br>";
+        return parent::getNom() . " mange de tout " . $nbreRepas . " fois par jour <br>";
     }
-    public function dormir() : void 
+    public function dormir(): void
     {
         echo "dort bcp <br>";
     }
-    public function courir(int $vitesse) : void
+    public function courir(int $vitesse): void
     {
-        echo parent::getNom(). " court à ".$vitesse." km/h <br>";
+        echo parent::getNom() . " court à " . $vitesse . " km/h <br>";
     }
 }
 
-class Aquatiques extends Animaux {
+class Aquatiques extends Animaux
+{
 
     public function manger(int $nbreRepas): string
     {
-        return parent::getNom(). " mange ce qu'il trouve ".$nbreRepas." fois par mois <br>";
+        return parent::getNom() . " mange ce qu'il trouve " . $nbreRepas . " fois par mois <br>";
     }
-    public function dormir() : void 
+    public function dormir(): void
     {
         echo "dort pas bcp <br>";
     }
-    public function nager(int $vitesse) : void 
+    public function nager(int $vitesse): void
     {
-        echo "nage à ".$vitesse;
+        echo "nage à " . $vitesse;
     }
 }
